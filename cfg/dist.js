2de5e005c4cd1d99db97652e71a51602544d522e
unknown <zhanghongguang@360buyAD.local>
2018-01-25 20:57:28 +0800

    complete

--- cfg/dist.js
+++ cfg/dist.js
@@ -14,17 +14,17 @@ let config = Object.assign({}, baseConfig, {
   cache: false,
   devtool: 'sourcemap',
   plugins: [
-    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.DedupePlugin(), //用来检测相识的文件，将冗余从output中消除掉
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     }),
     new BowerWebpackPlugin({
       searchResolveModulesDirectories: false
     }),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.optimize.UglifyJsPlugin(),//用来压缩输出js的代码
+    new webpack.optimize.OccurenceOrderPlugin(),//按照引用频度来排序各个模块budler的id,引用的越频繁，且id值越短，以便达到减小文件大小的效果
+    new webpack.optimize.AggressiveMergingPlugin(),//用来优化生成的代码段,合并相识的代码段，提取公共部分等
+    new webpack.NoErrorsPlugin()//用来保障编译过程不能出错
   ],
   module: defaultSettings.getDefaultModules()
 });
